e50986b7b7f6641b7d24479e478945caebea59de
mathieu <mathieusuter@ahoo.com>
2026-06-22 13:57:09 +0200

    creation de la fonction init, remet touts les valeurs a 0 pour eviter les valeurs bizarres

---
+++ sources/init/init_game.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_game.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msuter <msuter@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/22 13:45:56 by msuter            #+#    #+#             */
+/*   Updated: 2026/06/22 13:56:14 by msuter           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/cub3d.h"
+
+void	img_init(t_image *img)
+{
+	img->mlx_img = NULL;
+	img->x = 0;
+	img->y = 0;
+}
+
+void	init_game(t_data *data)
+{
+	data->mlx_img = NULL;
+	data->mlx_ptr = NULL;
+	data->mlx_win = NULL;
+	img_init(&data->door);
+	img_init(&data->player);
+	img_init(&data->wall);
+	data->map.columns = 0;
+	data->map.exit = 0;
+	data->map.grid = NULL;
+	data->map.player = 0;
+	data->map.rows = 0;
+}
